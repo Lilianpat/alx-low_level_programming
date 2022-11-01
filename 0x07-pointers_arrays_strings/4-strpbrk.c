33378dce10862546ca0e4adae50108490035618b
Lilian Obuzor <lilianobuzorc@gmail.com>
2022-10-31 21:12:32 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,6 @@
   * _strpbrk - search a string for any of a set of bytes
   * @s: source string
   * @accept: accepted characters
-  *
   * Return: the string since the first found accepted character
   */
 
