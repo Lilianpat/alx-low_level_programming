14dbffd1cbb15776ceab4bafded06ca2de15acb2
Lilianpat <113841280+Lilianpat@users.noreply.github.com>
2022-12-06 04:54:52 +0100

    100-elf_header

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,21 +7,25 @@
 
 /**
  * main - program that copies the content of a file to another file
+ *
  * @argc: Counts the number of parameters that go into main
  * @argv: Pointer of array of pointers containing strings entering main
  * Return: Always 0 on (Success)
+ *
  * if the number of argument is not the correct one, exit with code 97
  * and print Usage: cp file_from file_to, followed by a new line,
  * on the POSIX standard error
+ *
  * if file_from does not exist, or if you can not read it, exit with
  * code 98 and print Error: Can't read from file NAME_OF_THE_FILE,
  * followed by a new line, on the POSIX standard error
+ *
  * if you can not close a file descriptor ,
  * exit with code 100 and print Error:
  * Can't close fd FD_VALUE, followed by a new line,
  * on the POSIX standard error
  */
-    int main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int fdfrom, fdto, checkr, checkw, checkc1, checkc2;
 	char buff[1024];
@@ -39,6 +43,7 @@
 	fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdto == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
 
 	while ((checkr = read(fdfrom, buff, 1024)) > 0)
 	{
